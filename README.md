e7ed464d64d18f7ffc5adc7423f2e2e44e36bb0a
Chatchai Wongdetsakul <chatchai.w@netpoleons.com>
2023-01-06 16:50:56 +0700

    Add script to generate root CA, intermediate CA, and certificate signed with these CAs

--- README.md
+++ README.md
@@ -2,11 +2,29 @@
 
 ### Generate Self-signed Certificate to use with Sonatype Nexus RM
 #### Generate PEM format certificate
-1. Edit cert_ext.cnf file
-1. Run cert-gen.sh
+1. Edit certs/01_cert_ext.cnf file
+1. Run cert-gen.sh, it will prompt for pass phase
+```
+Generating Root CA private key
+Enter pass phrase for ./certs/rootca.key:
+Verifying - Enter pass phrase for ./certs/rootca.key:
+Generating Root CA certificate
+Enter pass phrase for ./certs/rootca.key:
+Generating Intermediate CA private key
+Enter pass phrase for ./certs/intermediate.key:
+Verifying - Enter pass phrase for ./certs/intermediate.key:
+Generating Intermediate CSR
+Enter pass phrase for ./certs/intermediate.key:
+Signing Intermediate CA certificate
+Enter pass phrase for ./certs/rootca.key:
+Generating Server private key and CSR
+Signing Server certificate
+Enter pass phrase for ./certs/intermediate.key:
+Creating Full Chain
+```
 
 #### Convert certificate from PEM format to PKCS12 format
-``` openssl pkcs12 -export -in server.crt -inkey server.key -out jkeystore.p12 ```
+``` openssl pkcs12 -export -in certs/fullchain.crt -inkey certs/server.key -out jkeystore.p12 ```
 
 #### Convert certificate from PKCS12 format to JKS format
 ``` keytool -importkeystore -destkeystore jkeystore.jks -deststoretype PKCS12 -srcstoretype PKCS12 -srckeystore jkeystore.p12 ```
--- cert-gen.sh
+++ cert-gen.sh
@@ -1,30 +1,82 @@
 #!/bin/sh
-
-PATH="."
-SERVER_KEY="$PATH/server.key"
-SERVER_CSR="$PATH/server.csr"
-SERVER_CRT="$PATH/server.crt"
-EXTFILE="$PATH/cert_ext.cnf"
 OPENSSL_CMD="/usr/bin/openssl"
 
-# generating server key
-echo "Generating private key"
-$OPENSSL_CMD genrsa -out $SERVER_KEY 4096 2>/dev/null
+CERT_PATH="./certs"
+
+ROOT_CONFIG="$CERT_PATH/01_rootCA.cnf"
+INTERMEDIATE_CONFIG="$CERT_PATH/01_intermediate.cnf"
+EXTFILE="$CERT_PATH/01_cert_ext.cnf"
+
+ROOTCA_KEY="$CERT_PATH/rootca.key"
+ROOTCA_CRT="$CERT_PATH/rootca.crt"
+
+INTERMEDIATE_KEY="$CERT_PATH/intermediate.key"
+INTERMEDIATE_CSR="$CERT_PATH/intermediate.csr"
+INTERMEDIATE_CRT="$CERT_PATH/intermediate.crt"
+
+SERVER_KEY="$CERT_PATH/server.key"
+SERVER_CSR="$CERT_PATH/server.csr"
+SERVER_CRT="$CERT_PATH/server.crt"
+
+FULLCHAIN="$CERT_PATH/fullchain.pem"
+
+touch $CERT_PATH/root_index.txt
+touch $CERT_PATH/intermediate_index.txt
+
+echo 1000 > $CERT_PATH/root_serial
+echo 1000 > $CERT_PATH/intermediate_serial
+
+# Generate Root CA private key
+echo "Generating Root CA private key"
+$OPENSSL_CMD genrsa -aes256 -out $ROOTCA_KEY 4096 2>/dev/null
 if [ $? -ne 0 ] ; then
-   echo "ERROR: Failed to generate $SERVER_KEY"
+   echo "ERROR: Failed to generate $ROOTCA_KEY"
    exit 1
 fi
-
-# Generating Certificate Signing Request using config file
-echo "Generating Certificate Signing Request"
-$OPENSSL_CMD req -new -key $SERVER_KEY -out $SERVER_CSR -config $EXTFILE 2>/dev/null
+# Generate Root CA certifcate
+echo "Generating Root CA certificate"
+$OPENSSL_CMD req -config $ROOT_CONFIG -key $ROOTCA_KEY -new -x509 -days 7300 -sha256 -extensions v3_ca -out $ROOTCA_CRT 2>/dev/null
 if [ $? -ne 0 ] ; then
-   echo "ERROR: Failed to generate $SERVER_CSR"
-   exit 1
+   echo "ERROR: Failed to generate $ROOTCA_CRT"
 fi
 
-echo "Generating self signed certificate"
-$OPENSSL_CMD x509 -req -days 3650 -in $SERVER_CSR -signkey $SERVER_KEY -out $SERVER_CRT -extfile $EXTFILE 2>/dev/null
+# Generate Intermediate CA private key
+echo "Generating Intermediate CA private key"
+$OPENSSL_CMD genrsa -aes256 -out $INTERMEDIATE_KEY 2>/dev/null
+if [ $? -ne 0 ] ; then
+   echo "ERROR: Failed to generate $INTERMEDIATE_KEY"
+   exit 1
+fi
+# Generate Intermediate CSR
+echo "Generating Intermediate CSR"
+$OPENSSL_CMD req -config $INTERMEDIATE_CONFIG -new -sha256 -key $INTERMEDIATE_KEY -out $INTERMEDIATE_CSR 2>/dev/null
+if [ $? -ne 0 ] ; then
+   echo "ERROR: Failed to generate CSR $INTERMEDIATE_CSR"
+fi
+# Sign Intermediate CA certifcate
+echo "Signing Intermediate CA certificate"
+$OPENSSL_CMD ca -config $ROOT_CONFIG -extensions v3_intermediate_ca -days 3650 -notext -md sha256 -in $INTERMEDIATE_CSR -out $INTERMEDIATE_CRT -batch 2>/dev/null
+if [ $? -ne 0 ] ; then
+   echo "ERROR: Failed to sign $INTERMEDIATE_CRT"
+fi
+
+# Generating Server key and CSR
+echo "Generating Server private key and CSR"
+$OPENSSL_CMD req -config $EXTFILE -nodes -newkey rsa:2048 -sha256 -keyout $SERVER_KEY -out $SERVER_CSR 2>/dev/null
+if [ $? -ne 0 ] ; then
+   echo "ERROR: Failed to generate $SERVER_KEY or $SERVER_CSR"
+   exit 1
+fi
+# Sign Server certifcate
+echo "Signing Server certificate"
+$OPENSSL_CMD ca -config $INTERMEDIATE_CONFIG -extensions server_cert -extfile $EXTFILE -days 375 -notext -md sha256 -in $SERVER_CSR -out $SERVER_CRT -batch 2>/dev/null
 if [ $? -ne 0 ] ; then
    echo "ERROR: Failed to generate self-signed certificate file $SERVER_CRT"
+fi
+
+# Create full chain
+echo "Creating Full Chain"
+cat $SERVER_CRT $INTERMEDIATE_CRT $ROOTCA_CRT > $FULLCHAIN 2>/dev/null
+if [ $? -ne 0 ] ; then
+   echo "ERROR: Failed to create full chain certificate file $FULLCHAIN"
 fi
